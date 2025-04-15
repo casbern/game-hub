1b0efda316927a0050b37178fcd0b85630b4fa40
Cássia <cascrisbern@gmail.com>
2025-04-15 14:02:46 +0300

    fetch the games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,11 @@
 import axios from "axios"
 
+const apiKey = import.meta.env.VITE_RAWG_API_KEY
+
+
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '779a0a9e98084262b85371ea4cbed3b4',
+    key: apiKey,
   }
 })
